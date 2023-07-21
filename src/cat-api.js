e2223fa191891f23e90b8655aa180919c4e6b194
Julipusik <yuliuaboyko@gmail.com>
2023-07-21 17:01:33 +0300

    hw 10

---
+++ src/cat-api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+axios.defaults.headers.common["x-api-key"] = "live_FRcDbYjg6Y4Kc922QIsJimpj1kTw2JBrlWOFAsU0yrWGfuUHObQ4YC0uBtbMTw7q";
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+
+function fetchBreeds () {
+  return axios
+    .get(`breeds/`)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    })
+    .catch(() => {
+        Report.failure('Oops! Something went wrong! Try reloading the page!');
+    });
+}
+
+function fetchCatByBreed(breedId) {
+    return axios
+        .get(`/images/search?breed_ids=${breedId}`)
+        .then(response => {
+        if (response.status !== 200) {
+            throw new Error(response.status);
+        }
+            return response.data[0];
+    })
+        .catch(() => {
+        Report.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+};
+
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import SlimSelect from 'slim-select'
+import "slim-select/dist/slimselect.css";
+
+const selectEl = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
+fetchBreeds()
+    .then(data => {
+        selectEl.innerHTML = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
+        new SlimSelect({
+            select: selectEl
+        })
+        Loading.remove();
+    })
+    .catch(() => {
+        Report.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+    
+
+selectEl.addEventListener('change', evt => {
+    evt.preventDefault();
+    Loading.circle('Loading data, please wait...');
+    const breedSelectId = selectEl.value;
+    fetchCatByBreed(breedSelectId)
+        .then(cat => {
+            Loading.remove();
+            catInfo.innerHTML = `
+            <div class='thumb-pic'><img src="${cat.url}" alt="${cat.id}" width=400></div>
+		<div class='thumb'>
+		<h2>${cat.breeds[0].name}</h2>
+		<p>${cat.breeds[0].description}</p>
+		<p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
+		</div>`;
+    })
+    .catch(() => {
+        Report.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+})
