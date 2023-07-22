9a028c249bc8cc33d1fa5d2543e486fae6ad18c7
Julipusik <yuliuaboyko@gmail.com>
2023-07-22 17:58:48 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -8,27 +8,27 @@ const catInfo = document.querySelector('.cat-info');
 
 fetchBreeds()
     .then(data => {
+        selectEl.classList.add("is-hidden");
+        Loading.circle('Loading data, please wait...');
         selectEl.innerHTML = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
-        Loading.remove();
     })
     .catch(() => {
         Report.failure('Oops! Something went wrong! Try reloading the page!');
     })
+    .finally(() => {
+        Loading.remove();
+        selectEl.classList.remove("is-hidden");
+    })
     
 
 selectEl.addEventListener('change', evt => {
-    evt.preventDefault();
     Loading.circle('Loading data, please wait...');
-    selectEl.classList.add("is-hidden");
     catInfo.classList.add("is-hidden");
 
-    const breedSelectId = selectEl.value;
+    const breedSelectId = evt.target.value;
     fetchCatByBreed(breedSelectId)
         .then(cat => {
-            Loading.remove();
-            selectEl.classList.remove("is-hidden");
             catInfo.classList.remove("is-hidden");
-
             catInfo.innerHTML = `
             <div class='thumb-pic'><img src="${cat.url}" alt="${cat.id}" width=400></div>
 		<div class='thumb'>
@@ -37,7 +37,11 @@ selectEl.addEventListener('change', evt => {
 		<p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
 		</div>`;
     })
-    .catch(() => {
-        Report.failure('Oops! Something went wrong! Try reloading the page!');
+        .catch(() => {
+            Report.failure('Oops! Something went wrong! Try reloading the page!');
+            catInfo.classList.add("is-hidden");
+    })
+    .finally(() => {
+        Loading.remove();
     })
 })
